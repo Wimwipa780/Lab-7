ea85be91651927e156d76288d1aae88bd9c8fc4c
wimwipa780 <wimwipha4746@gmail.com>
2024-12-06 13:28:02 +0700

    Lab7_3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,11 +2,24 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char N = x-1;
+	
+    
+	if (65<=int (N)&&int (N)<=90){
+		return (N) ;
+	}
+	if (x-1=='@'){
+		return 'Z';
+	}
+	else{
+		return '0' ;
+	}
+	
+	
 }
 
 int main(){
-	//Test Case
+	
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
